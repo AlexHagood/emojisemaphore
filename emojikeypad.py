043cd5a3e2c5eae10051b390591f40652f108b9f
alex <alexander.hagood@wsu.edu>
2024-04-06 13:23:04 -0700

    little bit of python just to set up text to flag correspondence

---
+++ emojikeypad.py
@@ -0,0 +1,38 @@
+num2emoji = {
+    "1": "↙️",
+    "2": "⬇️",
+    "3": "↘️",
+    "4": "⬅️",
+    "6": "➡️",
+    "7": "↖️",
+    "8": "⬆️",
+    "9": "↗️"
+}
+
+#numpad keys to directions so I can easily punch in all the emojis for each letter
+
+#I'm just going to punch in all letters for translating
+alphabet = "1242728229262341718618191613474849464378798369937636"
+numerals = alphabet[0:18] + "18" # 1-9 is just A-I, K is 0??
+
+
+abcs = "abcdefghijklmnopqrstuvwxyz"
+abcs = abcs.upper()
+
+nums = "1234567890"
+
+textToFlag = {
+
+}
+
+for i in range(0,26):
+    textToFlag.update({abcs[i]: num2emoji[alphabet[i * 2]] + num2emoji[alphabet[i*2 + 1]]})
+for i in range(0,10):
+    textToFlag.update({nums[i]: num2emoji[numerals[i * 2]] + num2emoji[numerals[i*2 + 1]]})
+
+textToFlag.update({" " : "⬇️⬇️", "#" : "⬆️↗️"})
+
+
+
+print(textToFlag)
+    
